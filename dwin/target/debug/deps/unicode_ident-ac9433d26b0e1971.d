3dd2c00b95f572bc0d6afa551f15d640e760bb5b
Provectus <fiddlebert2020@gmail.com>
2026-02-02 14:57:21 -0500

    cargo cleaned

--- dwin/target/debug/deps/unicode_ident-ac9433d26b0e1971.d
+++
@@ -1,8 +0,0 @@
-/home/maximus/Documents/GitHub/dwin/dwin/target/debug/deps/unicode_ident-ac9433d26b0e1971.d: /home/maximus/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.22/src/lib.rs /home/maximus/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.22/src/tables.rs
-
-/home/maximus/Documents/GitHub/dwin/dwin/target/debug/deps/libunicode_ident-ac9433d26b0e1971.rlib: /home/maximus/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.22/src/lib.rs /home/maximus/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.22/src/tables.rs
-
-/home/maximus/Documents/GitHub/dwin/dwin/target/debug/deps/libunicode_ident-ac9433d26b0e1971.rmeta: /home/maximus/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.22/src/lib.rs /home/maximus/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.22/src/tables.rs
-
-/home/maximus/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.22/src/lib.rs:
-/home/maximus/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.22/src/tables.rs:
