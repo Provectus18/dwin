a5fd97263b76d64faaee1f5c6190477239af9192
Provectus <fiddlebert2020@gmail.com>
2026-02-02 14:47:34 -0500

    ADDED features thiserror and x11

---
+++ dwin/target/debug/build/x11-991640f4e4d32795/build_script_build-991640f4e4d32795.d
@@ -0,0 +1,5 @@
+/home/maximus/Documents/GitHub/dwin/dwin/target/debug/build/x11-991640f4e4d32795/build_script_build-991640f4e4d32795.d: /home/maximus/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/x11-2.21.0/build.rs
+
+/home/maximus/Documents/GitHub/dwin/dwin/target/debug/build/x11-991640f4e4d32795/build_script_build-991640f4e4d32795: /home/maximus/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/x11-2.21.0/build.rs
+
+/home/maximus/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/x11-2.21.0/build.rs:
