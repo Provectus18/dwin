8b84b5c01beabbcba52575e3e5af516a5346a362
Provectus <fiddlebert2020@gmail.com>
2026-02-02 16:07:24 -0500

    updated mod and main.rs

---
+++ dwin/src/dwin/mod.rs
@@ -0,0 +1,30 @@
+use thiserror::Error;
+
+use std::ffi::NulError;
+
+#[derive(Error, Debug)]
+pub enum DwinError {
+    #[error("display {0} not found")]
+    DisplayNotFound(String),
+
+    #[error("{0}")]
+    NulString(#[from] NulError),
+}
+
+pub struct Dwin {
+
+}
+
+impl Dwin {
+    pub fn new(_display_name: &str) -> Result<Self, DwinError> {
+        Ok(Dwin {})
+    }
+
+    pub fn init(&self) -> Result<(), DwinError> {
+        Ok(())
+    }
+
+    pub fn run(&self) {
+        println!("dwin running");
+    }
+}
--- dwin/src/main.rs
+++ dwin/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+mod dwin;
+
+use std::error::Error;
+
+//use dwin::Dwin;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let display_name = std::env::var("DISPLAY")?;
+
+    let wm = dwin::Dwin::new(&display_name)?;
+
+    wm.init()?;
+    wm.run();
+
+    Ok(())
 }
